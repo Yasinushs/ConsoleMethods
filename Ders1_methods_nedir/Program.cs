b2da536b924976ba24d61339a9700b4a4eca07a8
Yasinus <Yasinus@DESKTOP-29IUMJH>
2024-12-17 22:11:59 +0300

    method başlıkları eklendi

--- Ders1_methods_nedir/Program.cs
+++ Ders1_methods_nedir/Program.cs
@@ -4,7 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+         
+            //Method hakkında bilgileri yazar
+
+            //Method kodlamanın en temel yapılarını oluşturmaktadır
+
         }
     }
 }
