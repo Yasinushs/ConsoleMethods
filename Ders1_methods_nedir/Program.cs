c8be227282ef7a19023828fac7c2147d416fe6cd
Yasinus <Yasinus@DESKTOP-29IUMJH>
2024-12-17 23:21:50 +0300

    Ders1 için örnekler yapıldı.

--- Ders1_methods_nedir/Program.cs
+++ Ders1_methods_nedir/Program.cs
@@ -2,13 +2,51 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args)//ANA METHOD
         {
-         
-            //Method hakkında bilgileri yazar
-
-            //Method kodlamanın en temel yapılarını oluşturmaktadır
+            YaziGetir();
+            Tire();
+            Toplama();
+            Tire();
+            DiziListesi();
 
         }
+        static void YaziGetir()// Yardımcı method
+        {
+            Console.WriteLine("C# method konusuna hoşgeldiniz");
+
+        }
+        static void Toplama()// Yardımcı Method
+        {
+            int sayi1 = 5;
+            int sayi2 = 15;
+
+            Console.WriteLine(sayi1 + sayi2);
+
+
+        }
+        static void Tire()
+        {
+            Console.WriteLine("---------------------------------");
+        }
+        static void DiziListesi()
+        {
+            Random random = new Random();
+            int[] sayilar = new int[10];
+            for (int i = 0; i < sayilar.Length; i++)
+            {
+                sayilar[i] = random.Next(1, 1500);
+            }
+            foreach (int i in sayilar) 
+            {
+                Console.WriteLine(i);
+            }
+
+        }
+
+
+
+
+
     }
 }
