184e7d11ff20ecd56d2f078ea4d74022bd188444
resulturfent <rsltrfnt@gmail.com>
2024-12-17 22:22:28 +0300

    ilk dersler

---
+++ Ders1_2_parametre_nedir/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ders1_2_parametre_nedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
