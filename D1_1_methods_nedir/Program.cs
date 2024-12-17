e5b2bcb5495aa96cc248611260dd089e01a8bc0c
resulturfent <rsltrfnt@gmail.com>
2024-12-17 22:38:58 +0300

    dersler işlendi

---
+++ D1_1_methods_nedir/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace D1_1_methods_nedir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
