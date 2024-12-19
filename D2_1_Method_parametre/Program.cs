e1078f5daeeb5fbae98a7f507b500f8ac90c5c31
resulturfent <rsltrfnt@gmail.com>
2024-12-19 22:56:18 +0300

    Ders2 void, return method

---
+++ D2_1_Method_parametre/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace D2_1_Method_parametre
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            #region 1.soru
+
+            //int sayi1 = 10;
+            //int sayi2 = 5;
+
+            //int topla = sayi1 + sayi2;
+
+            //Console.WriteLine("2 sayının toplamı:" + topla);
+            SayilarinToplami(10,5);
+            #endregion
+
+            #region 2.soru
+            Console.WriteLine("---------------");
+            //int sayi3 = 11;
+            //int sayi4 = 35;
+
+            //int topla2 = sayi3+ sayi4;
+
+            //Console.WriteLine("2 sayının toplamı:" + topla2);
+            SayilarinToplami(11, 35);
+            #endregion
+
+        }
+
+
+        static void SayilarinToplami(int sayi1,int sayi2)//sayi1,sayi2 parametredir
+        {
+            int topla = sayi1 + sayi2;
+            Console.WriteLine("2 sayının toplamı:" + topla);
+        }
+    }
+}
---
+++ D2_4_void_ornek/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace D2_4_void_ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Kullanıcıdan alınan 2 adet sayıdan büyük olanı method ile hesaplayıp kullanıcıya gösteren void BuyukSayi adında method ile kodlayınız
+
+
+        }
+    }
+}
