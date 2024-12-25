1c334a677c86dfac008b88ac8b8986094d66bd4d
Yasinus <Yasinus@DESKTOP-29IUMJH>
2024-12-25 22:55:20 +0300

    bugfix

---
+++ Ders3_3_overload_methods/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Ders3_3_overload_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
