45d2f29bea2cbbc68522e0a5ad2f08a88b0a86b6
resulturfent <rsltrfnt@gmail.com>
2025-01-02 22:27:35 +0300

    added last lesson topic

---
+++ D4_1_methods_ornek/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace D4_1_methods_ornek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             
+            Bir ülkede elektrik fatura kategorilere göre ayrılmıştır
+
+            Eğer konut için bir elektrik faturası varsa
+                        
+            0(dahil)-100 kilowatt(kw) harcandıysa kw başına 1.45 kr 
+            100(dahil)-400 kilowatt(kw) harcandıysa kw başına 1.51 kr 
+            40(dahil)-700 kilowatt(kw) harcandıysa kw başına 1.55 kr 
+            770(dahil)-1200 kilowatt(kw) harcandıysa kw ilk 1000 için 1550 tl alınacak, 1000 kw üstündeki harcama içinde 1.60 kr 
+            1200(dahil)  ve üstünde kw harcandıysa ilk 1500kw için 2000 tl, 1500 ve üstü kw için kw  başına 1.95 kr 
+             olarak hesaplanacaktır
+
+            Eğer Sanayi için bir elektrik faturası varsa
+             
+            0(dahil)-100 kilowatt(kw) harcandıysa kw başına 1.25 kr 
+            100(dahil)-400 kilowatt(kw) harcandıysa kw başına 1.32 kr 
+            40(dahil)-700 kilowatt(kw) harcandıysa kw başına 1.43 kr 
+            770(dahil)-1200 kilowatt(kw) harcandıysa kw ilk 1000 için 1350 tl alınacak, 1000 kw üstündeki harcama içinde 1.54 kr 
+            1200(dahil)  ve üstünde kw harcandıysa ilk 1500kw için 1750 tl, 1500 ve üstü kw için kw  başına 1.85 kr 
+             olarak hesaplanacaktır
+
+             Eğer Ağır Sanayi için bir elektrik faturası varsa
+             
+            0(dahil)-100 kilowatt(kw) harcandıysa kw başına 1.21 kr 
+            100(dahil)-400 kilowatt(kw) harcandıysa kw başına 1.28 kr 
+            40(dahil)-700 kilowatt(kw) harcandıysa kw başına 1.39 kr 
+            770(dahil)-1200 kilowatt(kw) harcandıysa kw ilk 1000 için 1290 tl alınacak, 1000 kw üstündeki harcama içinde 1.50 kr 
+            1200(dahil)  ve üstünde kw harcandıysa ilk 1500kw için 1650 tl, 1500 ve üstü kw için kw  başına 1.79 kr 
+             olarak hesaplanacaktır
+             
+             
+             */
+
+
+        }
+    }
+}
