2acba331729dd317e8166842d04bd780c8bb4e97
resulturfent <rsltrfnt@gmail.com>
2025-01-06 22:22:46 +0300

    ilk ders

---
+++ Hoca_SQLServer/250106_select_where_operatorler.sql
@@ -0,0 +1,27 @@
+/*
+SQL Server Giriş dersleri
+SSMS=> SQL Server Management Studio=> Micro M ,Soft S=> MS-SQL Serever
+Visual Studio => MS
+.Net => Dot Net=> 
+ASP Web Form--
+ASP.Net MVC
+ASP.Net Core=>2012-> 4 dil
+MS-SQL Server 
+ASP.Net CORE API
+*/
+/*
+1-Oracle-> Postgre SQL,PL/SQL
+2-MS-SQL Server
+3-Mongo DB
+4-My-SQL
+5-SQLite
+6-
+SQL=> Structure Query Language=> Database bir Programlama dili değildir
+.
+
+*/
+
+
+
+
+
